81dd95fc30964d6768d161ce31366484e966cca6
AlexEmlnv <alex17prog@gmail.com>
2024-04-28 20:56:49 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/org/example/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package org.example.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int symbolIndex = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r Load: " + process[symbolIndex]);
+            if (symbolIndex == 3) {
+                symbolIndex = 0;
+            } else {
+                symbolIndex++;
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws  InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+}
