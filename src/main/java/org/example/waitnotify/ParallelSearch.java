c2fd389fdcde7a1d9e9e579583ebdb753bc8c788
AlexEmlnv <alex17prog@gmail.com>
2024-05-25 19:58:42 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/org/example/waitnotify/ParallelSearch.java
@@ -0,0 +1,41 @@
+package org.example.waitnotify;
+
+import org.example.sharedresources.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        consumer.interrupt();
+    }
+}
